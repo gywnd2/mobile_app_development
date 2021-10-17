51052331cf9c7e1fdcea27213f7c6c610d43b4fa
Hyo-jung Lee <leeexpert@cau.ac.kr>
2021-10-17 19:30:20 +0900

    About fragment

---
+++ app/src/main/java/com/example/hyojunglee20172979/FragmentOne.java
@@ -0,0 +1,33 @@
+package com.example.hyojunglee20172979;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.hyojunglee20172979.databinding.FragmentOneBinding;
+
+public class FragmentOne extends Fragment {
+    private FragmentOneBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding=FragmentOneBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding=null;
+    }
+}
---
+++ app/src/main/java/com/example/hyojunglee20172979/FragmentTwo.java
@@ -0,0 +1,33 @@
+package com.example.hyojunglee20172979;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.hyojunglee20172979.databinding.FragmentTwoBinding;
+
+public class FragmentTwo extends Fragment {
+    private FragmentTwoBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding=FragmentTwoBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding=null;
+    }
+}
