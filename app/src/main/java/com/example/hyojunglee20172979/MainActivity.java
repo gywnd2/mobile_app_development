a13167ccaeaf4bfc9fc6e662f44689e9ee1f1270
Hyo-jung Lee <leeexpert@cau.ac.kr>
2021-10-27 15:31:38 +0900

    HW06 - Intent

--- app/src/main/java/com/example/hyojunglee20172979/MainActivity.java
+++ app/src/main/java/com/example/hyojunglee20172979/MainActivity.java
@@ -1,31 +1,47 @@
 package com.example.hyojunglee20172979;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import androidx.fragment.app.FragmentActivity;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hyojunglee20172979.databinding.ActivityMainBinding;
 
-public class MainActivity extends FragmentActivity implements FragmentOne.onClickListener{
+public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    private static final String TAG="StateChange";
+    private ActivityResultLauncher<Intent> resultLauncher;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
         View view=binding.getRoot();
         setContentView(view);
 
-        Log.i(TAG, "onCreate");
+        resultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode()==RESULT_OK){
+                    Intent data=result.getData();
+
+                    String returnString=data.getExtras().getString("returnData");
+                    binding.textview1.setText(returnString);
+                }
+            }
+        });
     }
 
-    @Override
-    public void onButtonClick(String text) {
-        FragmentTwo fragmentTwo=(FragmentTwo) getSupportFragmentManager().findFragmentById(R.id.frag2);
+    public void sendText(View view){
+        Intent intent=new Intent(this, SecondActivity.class);
 
-        fragmentTwo.changeText(text);
+        String myString=binding.editText1.getText().toString();
+        intent.putExtra("qString", myString);
+        resultLauncher.launch(intent);
     }
 }
