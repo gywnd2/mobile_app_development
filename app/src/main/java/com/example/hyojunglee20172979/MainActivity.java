aa3394e0215fb2af3cdce46e9f2f6829e9ccc1cf
Hyo-jung Lee <leeexpert@cau.ac.kr>
2021-11-20 01:41:53 +0900

    HW09 - Record and play an audio

--- app/src/main/java/com/example/hyojunglee20172979/MainActivity.java
+++ app/src/main/java/com/example/hyojunglee20172979/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.hyojunglee20172979;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.Manifest;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,9 +14,17 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hyojunglee20172979.databinding.ActivityMainBinding;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.normal.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private MediaPlayer mediaPlayer;
+    private MediaRecorder mediaRecorder;
+    private String filename;
+    private SurfaceHolder surfaceHolder;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,68 +32,98 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        setTitle("Movie Information");
+
+        TedPermission.create().setPermissionListener(permissionlistener)
+                .setDeniedMessage("Permissions are Denied.")
+                .setPermissions(Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
+
+        SurfaceView surfaceView = new SurfaceView(this);
+
+        surfaceHolder = surfaceView.getHolder();
+
+        binding.container.addView(surfaceView);
+
+        filename = this.getExternalFilesDir(Environment.DIRECTORY_MOVIES).getAbsolutePath()+"/recorded.mp4";
     }
 
-    public class myDBHelper extends SQLiteOpenHelper {
-        public myDBHelper(Context context) {
-            super(context, "groupDB", null, 1);
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
         }
 
         @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("create table groupTBL (mName char(30), mDirector char(20), mYear char(20));");
+        public void onPermissionDenied(List<String> deniedPermissions) {
+            Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
         }
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("drop table if exists groupTBL;");
-            onCreate(db);
+    };
+
+
+    public void startRecording(View view){
+
+        if(mediaPlayer!=null){
+            mediaPlayer.release();
+            mediaPlayer=null;
+        }
+
+        if(mediaRecorder == null) {
+            mediaRecorder = new MediaRecorder();
+
+        }
+
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+        mediaRecorder.setOutputFile(filename);
+        mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+
+        try{
+            mediaRecorder.prepare();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        mediaRecorder.start();
+    }
+
+    public void stopRecording(View view){
+
+        if(mediaRecorder == null){
+            return ;
+        }
+
+        mediaRecorder.stop();
+        mediaRecorder.reset();
+        mediaRecorder.release();
+        mediaRecorder = null;
+
+    }
+
+    public void startPlay(View view){
+        if(mediaPlayer==null){
+            mediaPlayer = new MediaPlayer();
+        }
+
+        try{
+            mediaPlayer.setDataSource(filename);
+            mediaPlayer.setDisplay(surfaceHolder);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 
-    public void initializeDB(View view) {
-        myDBHelper myHelper = new myDBHelper(this);
-        SQLiteDatabase sqlDB = myHelper.getWritableDatabase();
-        myHelper.onUpgrade(sqlDB, 1, 2);
-        sqlDB.close();
-        Toast.makeText(getApplicationContext(), "Initialized", Toast.LENGTH_LONG).show();
-        ;
-    }
+    public void stopPlay(View view){
 
-    public void insertDB(View view) {
-        myDBHelper myHelper = new myDBHelper(this);
-        SQLiteDatabase sqlDB = myHelper.getWritableDatabase();
-        sqlDB.execSQL("insert into groupTBL values('" + binding.textinputMovietitle.getText().toString() + "','" + binding.textinputDirector.getText().toString() + "','" + binding.textinputReleasedyear.getText().toString() + "');");
-        sqlDB.close();
-        Toast.makeText(getApplicationContext(), "Inserted", Toast.LENGTH_LONG).show();
-    }
-
-    public void searchDB(View view) {
-        myDBHelper myHelper = new myDBHelper(this);
-        SQLiteDatabase sqlDB = myHelper.getWritableDatabase();
-        Cursor cursor;
-        cursor = sqlDB.rawQuery("select * from groupTBL;", null);
-
-        String string1 = "Movie Title" + System.lineSeparator();
-        String string2 = "Director" + System.lineSeparator();
-        String string3 = "Released Year" + System.lineSeparator();
-
-        string1 += "-----------" + System.lineSeparator();
-        string2 += "-----------" + System.lineSeparator();
-        string3 += "-----------" + System.lineSeparator();
-
-        while (cursor.moveToNext()) {
-            string1 += cursor.getString(0) + System.lineSeparator();
-            string2 += cursor.getString(1) + System.lineSeparator();
-            string3 += cursor.getString(2) + System.lineSeparator();
+        if(mediaPlayer !=null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer=null;
         }
-
-        binding.textTitleColumnOne.setText(string1);
-        binding.textTitleColumnTwo.setText(string2);
-        binding.textTitleColumnThree.setText(string3);
-
-        cursor.close();
-        sqlDB.close();
     }
 }
